14b84c8bfdf8e993449df0b0b904e32e15d8aec8
Poliklot <poliklot@vk.com>
2025-07-04 11:13:07 +0300

    update template

--- src/data/index.mjs
+++ src/data/index.mjs
@@ -1,13 +1,4 @@
-import { productsDB } from './productsDB.js';
-
 export default {
 	projectName: 'projectName',
-	copyright: 'copyright',
-	smallSliderProductCards: [
-		productsDB[874],
-		productsDB[8711],
-		productsDB[8711],
-		productsDB[874],
-		productsDB[8711],
-	]
+	copyright: 'copyright'
 }
